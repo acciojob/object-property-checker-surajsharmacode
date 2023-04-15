4ce0ac69e2005f12befebcdbd5e718beefbe6547
suraj sharma <94784024+surajsharmacode@users.noreply.github.com>
2023-04-15 22:36:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
+	if(key in sampleObject){
+		return true;
+	}else{
+		return false;
+	}
   //   write your code here
 }
 
-// Do not change the code below
+//Do not change the code below
 const key = prompt("Enter Key.");
 alert(hasKey(key));
